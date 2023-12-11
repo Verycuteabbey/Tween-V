ff9aae81ff4bbc9b9e990c3af1e22b062d77b75a
VoID <mengmengyujie@hotmail.com>
2023-12-12 00:02:28 +0800

    写好基本部分力

--- src/controller.lua
+++ src/controller.lua
@@ -7,19 +7,20 @@
 
 
     Author // VoID(@Verycuteabbey)
-    Contributor // ChinoHTea(@HarukaTea), smallpenguin666
 ]]--
 
 --// defines
 type easeStyle = "Linear" | "Quad" | "Cubic" | "Quart" | "Quint" | "Sine" | "Expo" | "Circ" | "Elastic" | "Back" | "Bounce";
 type easeDirection = "In" | "Out" | "InOut";
-type positionType = CFrame | Color3 | number | Vector2 | Vector3 | UDim2;
+type positionType = CFrame | Color3 | ColorSequenceKeypoint | DateTime | number | NumberRange | NumberSequenceKeypoint | Ray | Rect | Region3 | UDim2 | Vector2 | Vector3;
 
-local RunService = game:GetService("RunService");
+local library: table = require(script.Parent);
+local runService: RunService = game:GetService("RunService");
 
-local library: table = {};
+local controller: table = {};
 --// functions
-function library:Create(easeOption: {style: easeStyle?, direction: easeDirection?, duration: number?}?, A: positionType, B: positionType): table
+function controller:Create(instance: Instance, property: string, easeOption: {style: easeStyle?, direction: easeDirection?, duration: number?}?, target: positionType): table
+    --#region // default
     if (not easeOption) then
         warn("Tween-V - Warning // empty easeOptions has been given, using default");
 
@@ -41,12 +42,118 @@ function library:Create(easeOption: {style: easeStyle?, direction: easeDirection
 
         easeOption.duration = 1;
     end;
+    --#endregion
+    local object: table = {};
 
-    local object = {};
+    local meta: table = {};
+    meta.__index = meta;
 
-    object.easeOption = easeOption;
+    meta.instance = {
+        self = instance;
+        property = property;
+        recentValue = instance[property];
+        target = target;
+        easeOption = easeOption;
+    };
+    meta.options = {
+        recycle = false;
+        timer = 60;
+        yield = false;
+        threads = {};
+    };
+    --#region // Continue
+    function object:Continue()
+        meta = getmetatable(self) :: table;
 
+        local options: table = meta.options;
+
+        options.yield = false;
+        setmetatable(self, meta);
+    end;
+    --#endregion
+    --#region // Replay
+    function object:Replay()
+        meta = getmetatable(self) :: table;
+
+        local options: table = meta.options;
+
+        local threads: table = options.threads;
+
+        if (not options.tween) then return end;
+
+        threads[2]:Disconnect();
+        local connection: RBXScriptConnection = runService.Heartbeat:Connect(options.tween);
+        threads[2] = connection;
+
+        setmetatable(self, meta);
+    end;
+    --#endregion
+    --#region // Start
+    function object:Start()
+        meta = getmetatable(self) :: table;
+
+        local instance: table = meta.instance;
+        local options: table = meta.options;
+
+        local threads: table = options.threads;
+        --#region // timer
+        local timer: number = options.timer;
+
+        local function main()
+            local recycleTime: number = 0;
+
+            while (task.wait(1)) do
+                if (timer > 0) then
+                    timer -= 1;
+                elseif (recycleTime < 10) then
+                    recycleTime += 1;
+                else
+                    setmetatable(self, {});
+                    self = nil;
+                    break;
+                end;
+            end;
+        end;
+
+        local thread: thread = task.spawn(main);
+        threads[#threads + 1] = thread;
+        --#endregion
+        --#region // tween
+        local nowTime: number = 0;
+
+        local function tween(deltaTime: number)
+            if (options.yield) then return end;
+
+            if (nowTime > easeOption.duration) then
+                meta = getmetatable(self);
+                threads[2]:Disconnect();
+                nowTime = easeOption.duration;
+            end;
+
+            instance.self[instance.property] = library:Lerp(easeOption, instance.recentValue, instance.target, nowTime / easeOption.duration);
+            nowTime += deltaTime;
+            timer = 60;
+        end;
+
+        local connection: RBXScriptConnection = runService.Heartbeat:Connect(tween);
+        threads[#threads + 1] = connection;
+        options.tween = tween;
+        --#endregion
+        setmetatable(self, meta);
+    end;
+    --#endregion
+    --#region // Stop
+    function object:Stop()
+        meta = getmetatable(self) :: table;
+
+        local options: table = meta.options;
+
+        options.yield = true;
+        setmetatable(self, meta);
+    end;
+    --#endregion
+    setmetatable(object, meta);
     return object;
 end;
 
-return library;
+return controller;
