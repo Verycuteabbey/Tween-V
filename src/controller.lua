a0b08eeae256b22c8e54905accc89068e40deb6d
Verycuteabbey <mengmengyujie@hotmail.com>
2024-02-19 12:43:12 +0800

    再次尝试新逻辑

--- src/controller.lua
+++ src/controller.lua
@@ -11,19 +11,24 @@
 ]]--
 
 --// defines
-local cancel, delay, spawn, wait = task.cancel, task.delay, task.spawn, task.wait
+local cancel, defer, delay, spawn, wait = task.cancel, task.defer, task.delay, task.spawn, task.wait
 local resume, yield = coroutine.resume, coroutine.yield
 
+type tweens = { table }
+
+local active: tweens = {}
+local suspended: tweens = {}
+
+--// libraries
 local controller = {}
 
 --// libraries
-local parent = script.Parent
-local library = require(parent.Library)
+local library = require()
 
 --// functions
 function controller:Create(
     instance: Instance,
-    easeOption: { [number]: Enum | number | string | table },
+    easeOption: { Enum | number | string | table },
     target: table,
     schedule: number?
 ): table
@@ -37,6 +42,7 @@ function controller:Create(
 	object.status = {
 		ended = false,
 		looped = 0,
+		nowTime = schedule,
 		reversed = false,
 		started = false,
 		yielding = false
@@ -52,20 +58,16 @@ function controller:Create(
 	--#region // function - Kill(_delay: number?)
 	function funcs:Kill(_delay: number?)
 		local status = object.status
-		local thread = object.thread
 
 		if not status.started then return end
 
 		delay(_delay, function()
 			status.ended = true
-
-			cancel(thread)
 		end)
 	end
 	--#endregion
 	--#region // function - Replay(_delay: number?, _repeat: number?, reverse: boolean?)
 	function funcs:Replay(_delay: number?, _repeat: number?, reverse: boolean?)
-		--#region // init
 		local status = object.status
 
 		if not status.started then return end
@@ -73,6 +75,7 @@ function controller:Create(
 		object.status = {
             ended = false,
             looped = 0,
+			nowTime = schedule,
             reversed = false,
             started = false,
             yielding = false
@@ -84,93 +87,28 @@ function controller:Create(
 		info._delay = _delay
 		info._repeat = _repeat
 		info.reverse = reverse
-		--#endregion
-		--#region // tween
-		local current = info.current
-		local duration = easeOption[3] :: number
 
-        local nowTime = schedule :: number
-
-		local function __tween()
-			while true do
-				status = object.status
-
-				if not instance then return end
-				if status.yielding then yield() end
-
-				if nowTime > duration then
-					if reverse and not status.reversed then
-						status.reversed = true
-
-						local temp = current
-						current = target
-						target = temp
-
-						nowTime = schedule
-					elseif status.looped < _repeat or _repeat == -1 then
-						status.looped += 1
-
-						if status.reversed then
-							status.reversed = false
-
-							local temp = current
-							current = target
-							target = temp
-						end
-
-						nowTime = schedule
-					else
-						status.ended = true
-						nowTime = duration
-					end
-				end
-
-				for K, V in pairs(target) do
-					local variant = library:Lerp(easeOption, current[K], V, nowTime / duration)
-					instance[K] = variant
-				end
-
-				if status.ended and status.reversed then
-					status.reversed = false
-
-					local temp = current
-					current = target
-					target = temp
-
-					return
-				end
-
-				local deltaTime = wait()
-				nowTime += deltaTime
-			end
-		end
-		--#endregion
 		delay(_delay, function()
-			object.thread = spawn(__tween) :: thread
+			status.started = true
 		end)
 	end
 	--#endregion
 	--#region // function - Resume(_delay: number?)
 	function funcs:Resume(_delay: number?)
 		local status = object.status
-		local thread = object.thread
 
-		if not status.started or status.killed then return end
+		if not status.started then return end
 
 		delay(_delay, function()
 			status.yielding = false
-
-			resume(thread)
 		end)
 	end
 	--#endregion
 	--#region // function - Start(_delay: number?, _repeat: number?, reverse: boolean?)
 	function funcs:Start(_delay: number?, _repeat: number?, reverse: boolean?)
-		--#region // init
 		local status = object.status
 
 		if status.started then return end
-		status.started = true
 
 		_delay = _delay or 0
 		_repeat = _repeat or 0
@@ -178,69 +116,69 @@ function controller:Create(
 		info._delay = _delay
 		info._repeat = _repeat
 		info.reverse = reverse
-		--#endregion
-		--#region // tween
+
+		delay(_delay, function()
+			status.started = true
+		end)
+	end
+	--#endregion
+	--#region // function - Update(deltaTime: number)
+	function funcs:Update(deltaTime: number)
+		local status = object.status
+
+		if not status.started or status.ended then return end
+
 		local current = info.current
 		local duration = easeOption[3] :: number
+		local nowTime = status.nowTime
+		local reverse, _repeat = info.reverse, info._repeat
 
-        local nowTime = schedule :: number
+		spawn(function()
+			if not instance then return end
 
-		local function __tween()
-			while true do
-				status = object.status
-
-				if not instance then return end
-				if status.yielding then yield() end
-
-				if nowTime > duration then
-					if reverse and not status.reversed then
-						status.reversed = true
-
-						local temp = current
-						current = target
-						target = temp
-
-						nowTime = schedule
-					elseif status.looped < _repeat or _repeat == -1 then
-						status.looped += 1
-
-						if status.reversed then
-							status.reversed = false
-
-							local temp = current
-							current = target
-							target = temp
-						end
-
-						nowTime = schedule
-					else
-						status.ended = true
-						nowTime = duration
-					end
-				end
-
-				for K, V in pairs(target) do
-					local variant = library:Lerp(easeOption, current[K], V, nowTime / duration)
-					instance[K] = variant
-				end
-
-				if status.ended and status.reversed then
-					status.reversed = false
+			if nowTime > duration then
+				if reverse and not status.reversed then
+					status.reversed = true
 
 					local temp = current
 					current = target
 					target = temp
 
-					return
-				end
+					nowTime = schedule
+				elseif status.looped < _repeat or _repeat == -1 then
+					status.looped += 1
 
-				local deltaTime = wait()
-				nowTime += deltaTime
+					if status.reversed then
+						status.reversed = false
+
+						local temp = current
+						current = target
+						target = temp
+					end
+
+					nowTime = schedule
+				else
+					status.ended = true
+					nowTime = duration
+				end
 			end
-		end
-		--#endregion
-		delay(_delay, function()
-			object.thread = spawn(__tween) :: thread
+
+			for K, V in pairs(target) do
+				local variant = library:Lerp(easeOption, current[K], V, nowTime / duration)
+				instance[K] = variant
+			end
+
+			if status.ended and status.reversed then
+				status.reversed = false
+
+				local temp = current
+				current = target
+				target = temp
+
+				return
+			end
+
+			nowTime += deltaTime
 		end)
 	end
 	--#endregion
@@ -256,6 +194,7 @@ function controller:Create(
 	end
 	--#endregion
 	setmetatable(object, funcs)
+	active[#active + 1] = object
 	return object
 end
 
@@ -267,5 +206,59 @@ function controller:EaseOption(
 ): table
 	return library:EaseOption(style, direction, duration, extra)
 end
+
+spawn(function()
+	while true do
+		local deltaTime = wait()
+
+		for position, tween: table in pairs(active) do
+			local status = tween.status
+
+			if status.yielding then
+				suspended[#suspended + 1] = tween
+				active[position] = nil
+			else
+				tween:Update(deltaTime)
+			end
+		end
+	end
+end)
+
+defer(function()
+	while true do
+		for position, tween: table in pairs(suspended) do
+			local status = tween.status
+
+			if not status.yielding then
+				active[#active + 1] = tween
+				suspended[position] = nil
+			end
+		end
+
+		wait()
+	end
+end)
+
+defer(function()
+	while true do
+		for position, tween: table in pairs(active) do
+			local status = tween.status
+
+			if status.ended then
+				active[position] = nil
+			end
+		end
+
+		for position, tween: table in pairs(suspended) do
+			local status = tween.status
+
+			if status.ended then
+				active[position] = nil
+			end
+		end
+
+		wait(60)
+	end
+end)
 
 return controller
