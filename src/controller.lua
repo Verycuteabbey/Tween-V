b900e98f04f63b207c2276429793e07e999aef41
Verycuteabbey <mengmengyujie@hotmail.com>
2024-02-25 01:19:08 +0800

    重写

--- src/controller.lua
+++ src/controller.lua
@@ -10,147 +10,157 @@
     Contributor // ChinoHTea(@HarukaTea), smallpenguin666
 ]]--
 
---// defines
-local defer, delay, spawn, wait = task.defer, task.delay, task.spawn, task.wait
-
-type tweens = { table }
-
-local active: tweens = {}
-local suspended: tweens = {}
-
 --// libraries
 local controller = {}
-
---// libraries
 local library = require()
 
---// functions
-function controller:Create(
-    instance: Instance,
-    easeOption: { Enum | number | string | table },
-    target: table,
-    schedule: number?
-): table
-    --#region // init
-    schedule = schedule or 0
+--// defines
+local deferTask, delayTask, spawnTask, waitTask = task.defer, task.delay, task.spawn, task.wait
 
-    local object = {}
-    object.info = {
-        current = {}
+type object = { [string]: table }
+type target = { [string]: library.source }
+type tweens = { table }
+
+local tweens: tweens = {}
+
+--// functions
+function controller:Create(instance: Instance, easeOption: library.easeOption, target: target, schedule: number?): object
+    --#region // init
+    schedule = schedule :: number or 0
+
+    local object: object = {}
+    object.properties = {
+        cycles = 0,
+        reverse = false,
+        saved = {}
     }
     object.status = {
+        cycled = 0,
+        flagged = false,
         ended = false,
-        looped = 0,
-        nowTime = schedule,
         reversed = false,
+        schedule = schedule,
         started = false,
         yielding = false
     }
 
+    local properties = object.properties
+
+    for property, _ in pairs(target) do
+        if not instance then return end
+
+        properties.saved = instance[property]
+    end
+    --#endregion
+    --#region // functions
     local funcs = {}
     funcs.__index = funcs
 
-    local info = object.info
-
-    for K, _ in pairs(target) do info.current[K] = instance[K] end
-    --#endregion
-    --#region // function - Kill(_delay: number?)
-    function funcs:Kill(_delay: number?)
+    --#region // Kill(delay: number?)
+    function funcs:Kill(delay: number)
         local status = object.status
 
-        if not status.started then return end
+        delay = delay :: number or 0
 
-        delay(_delay, function()
+        delayTask(delay, function()
             status.ended = true
         end)
     end
     --#endregion
-    --#region // function - Replay(_delay: number?, _repeat: number?, reverse: boolean?)
-    function funcs:Replay(_delay: number?, _repeat: number?, reverse: boolean?)
+    --#region // Restart(delay: number?, cycles: number?, reverse: boolean)
+    function funcs:Restart(delay: number?, cycles: number?, reverse: boolean)
         local status = object.status
+        local reversed, started = status.reversed, status.started
+        local saved = properties.saved
 
-        if not status.started then return end
+        if not started then return end
+        if reversed then
+            local temp = saved
+            properties.stored = target
+            target = temp
+        end
 
         object.status = {
+            cycled = 0,
+            flagged = false,
             ended = false,
-            looped = 0,
-            nowTime = schedule,
             reversed = false,
+            schedule = schedule,
             started = false,
             yielding = false
         }
 
-        _delay = _delay or info._delay
-        _repeat = _repeat or info._repeat
-        reverse = reverse or info.reverse
-        info._delay = _delay
-        info._repeat = _repeat
-        info.reverse = reverse
+        properties.delay = delay :: number or 0
+        properties.cycles = cycles :: number or 0
+        properties.reverse = reverse :: boolean or false
 
+        delay = properties.delay
         status = object.status
 
-        delay(_delay, function()
+        delayTask(delay, function()
             status.started = true
         end)
     end
     --#endregion
-    --#region // function - Resume(_delay: number?)
-    function funcs:Resume(_delay: number?)
+    --#region // Resume(delay: number?)
+    function funcs:Resume(delay: number?)
         local status = object.status
 
-        if not status.started then return end
+        delay = delay :: number or 0
 
-        delay(_delay, function()
+        delayTask(delay, function()
             status.yielding = false
         end)
     end
     --#endregion
-    --#region // function - Start(_delay: number?, _repeat: number?, reverse: boolean?)
-    function funcs:Start(_delay: number?, _repeat: number?, reverse: boolean?)
+    --#region // Start(delay: number?, cycles: number?, reverse: boolean)
+    function funcs:Start(delay: number?, cycles: number?, reverse: boolean)
         local status = object.status
+        local started = status.started
 
-        if status.started then return end
+        if started then return end
 
-        _delay = _delay or 0
-        _repeat = _repeat or 0
-        reverse = reverse or false
-        info._delay = _delay
-        info._repeat = _repeat
-        info.reverse = reverse
+        properties.delay = delay :: number or 0
+        properties.cycles = cycles :: number or 0
+        properties.reverse = reverse :: boolean or false
 
-        delay(_delay, function()
+        delay = properties.delay
+
+        delayTask(delay, function()
+            status.flagged = false
             status.started = true
         end)
     end
     --#endregion
-    --#region // function - Update(deltaTime: number)
+    --#region // Update(deltaTime: number)
     function funcs:Update(deltaTime: number)
         local status = object.status
+        local started, ended = status.started, status.ended
 
-        if not status.started or status.ended then return end
+        if not started or ended then return end
+        if not instance then status.ended = true end
 
+        local cycled, reversed, schedule = status.cycled, status.reversed, status.schedule
         local duration = easeOption[3] :: number
-        local reverse, _repeat = info.reverse, info._repeat
+        local cycles, reverse, saved = properties.cycles, properties.reverse, properties.saved
 
-        if not instance then return end
-
-        if status.nowTime > duration then
-            if reverse and not status.reversed then
+        if schedule > duration then
+            if reverse and not reversed then
                 status.reversed = true
-                
-                local temp = info.current
-                info.current = target
+
+                local temp = saved
+                properties.saved = target
                 target = temp
 
                 status.nowTime = schedule
-            elseif status.looped < _repeat or _repeat == -1 then
-                status.looped += 1
+            elseif cycled < cycles or cycles == -1 then
+                status.cycled += 1
 
                 if status.reversed then
                     status.reversed = false
 
-                    local temp = info.current
-                    info.current = target
+                    local temp = saved
+                    properties.saved = target
                     target = temp
                 end
 
@@ -161,100 +171,108 @@ function controller:Create(
             end
         end
 
-        for K, V in pairs(target) do
-            local variant = library:Lerp(easeOption, info.current[K], V, status.nowTime / duration)
-            instance[K] = variant
-        end
-
-        if status.ended and status.reversed then
-            status.reversed = false
-
-            local temp = info.current
-            info.current = target
-            target = temp
-
-            return
+        for property, value in pairs(target) do
+            local variant = library:Lerp(easeOption, saved[property], value, schedule / duration)
+            instance[property] = variant
         end
 
         status.nowTime += deltaTime
     end
     --#endregion
-    --#region // function - Yield(_delay: number?)
-    function funcs:Yield(_delay: number?)
+    --#region // Yield(delay: number?, duration: number?)
+    function funcs:Yield(delay: number?, duration: number?)
         local status = object.status
+        local started = status.started
 
-        if not status.started then return end
+        if not started then return end
 
-        delay(_delay, function()
+        delay = delay :: number or 0
+        duration = duration :: number or 0
+
+        delayTask(delay, function()
             status.yielding = true
+
+            if duration == 0 then return end
+
+            local count = 0
+
+            spawnTask(function()
+                while true do
+                    waitTask(1)
+
+                    if count >= duration then break end
+
+                    count += 1
+                end
+
+                status.yielding = false
+            end)
         end)
     end
     --#endregion
     setmetatable(object, funcs)
-    active[#active + 1] = object
+    --#endregion
+    tweens[#tweens + 1] = object
     return object
 end
 
-function controller:EaseOption(
-    style: string | Enum.EasingStyle?,
-    direction: string | Enum.EasingDirection?,
+function library:EaseOption(
+	style: string | Enum.EasingStyle?,
+	direction: string | Enum.EasingDirection?,
     duration: number?,
-    extra: { amplitude: number?, period: number? }?
-): table
-    return library:EaseOption(style, direction, duration, extra)
+	extra: library.extra?
+): library.easeOption
+	local default = library.default
+
+	if extra then
+		extra.amplitude = extra.amplitude or default.extra.amplitude
+		extra.period = extra.period or default.extra.period
+	end
+
+    style = if typeof(style) == "EnumItem" then style.Name else style :: string
+	direction = if typeof(direction) == "EnumItem" then direction.Name else direction :: string
+
+	return {
+		[1] = style :: string or default.style,
+		[2] = direction :: string or default.direction,
+        [3] = duration :: number or default.duration,
+		[4] = extra :: library.extra or default.extra
+	}
 end
 
-spawn(function()
+spawnTask(function()
     while true do
-        local deltaTime = wait()
+        local deltaTime = waitTask()
 
-        for position, tween: table in pairs(active) do
+        for _, tween in pairs(tweens) do
             local status = tween.status
+            local yielding = status.yielding
 
-            if status.yielding then
-                suspended[#suspended + 1] = tween
-                active[position] = nil
-            else
+            if not yielding then
                 tween:Update(deltaTime)
             end
         end
     end
 end)
 
-defer(function()
+deferTask(function()
     while true do
-        for position, tween: table in pairs(suspended) do
-            local status = tween.status
+        waitTask(60)
 
-            if not status.yielding then
-                active[#active + 1] = tween
-                suspended[position] = nil
+        for position, tween in pairs(tweens) do
+            local status = tween.status
+            local flagged, started, ended = status.flagged, status.started, status.ended
+
+            if not started and not flagged then
+                status.flagged = true
+            elseif flagged then
+                tweens[position] = nil
+            end
+
+            if ended then
+                tweens[position] = nil
             end
         end
-
-        wait()
-    end
-end)
-
-defer(function()
-    while true do
-        for position, tween: table in pairs(active) do
-            local status = tween.status
-
-            if not status.started or status.ended then
-                active[position] = nil
-            end
-        end
-
-        for position, tween: table in pairs(suspended) do
-            local status = tween.status
-
-            if status.ended then
-                active[position] = nil
-            end
-        end
-
-        wait(60)
     end
 end)
 
